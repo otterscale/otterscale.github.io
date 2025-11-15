0d4760edb948fa3ace6dbdf7a715f79c7009e128
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-15 18:00:44 +0800

    feat: enhance Starlight integration with OpenAPI support and update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://otterscale.com',
 	integrations: [
 		starlight({
-			title: 'OtterScale Docs',
+			title: 'OtterScale',
 			logo: {
 				src: './src/assets/logo.png'
 			},
@@ -19,6 +20,17 @@ export default defineConfig({
 					href: 'https://github.com/otterscale/otterscale'
 				}
 			],
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en'
+				},
+				'zh-hant': {
+					label: '繁體中文',
+					lang: 'zh-Hant'
+				}
+			},
 			editLink: {
 				baseUrl: 'https://github.com/otterscale/otterscale.github.io/edit/main/docs/'
 			},
@@ -33,9 +45,20 @@ export default defineConfig({
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' }
-				}
+				},
+				...openAPISidebarGroups
 			],
-			customCss: ['./src/styles/global.css']
+			customCss: ['./src/styles/global.css'],
+			plugins: [
+				// Generate the OpenAPI documentation pages.
+				starlightOpenAPI([
+					{
+						base: 'api',
+						schema:
+							'https://raw.githubusercontent.com/otterscale/otterscale/refs/heads/main/api/openapi.yaml'
+					}
+				])
+			]
 		})
 	],
 	vite: {
