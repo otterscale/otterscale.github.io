c620c70c076aeabc3f5f912ec765da33bc5df0c7
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-15 17:25:33 +0800

    feat: configure starlight integration and add logo assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://otterscale.com',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://otterscale.com",
+  integrations: [
+    starlight({
+      title: "OtterScale Docs",
+      logo: {
+        src: "./src/assets/logo.png",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/otterscale/otterscale",
+        },
+      ],
+      editLink: {
+        baseUrl:
+          "https://github.com/otterscale/otterscale.github.io/edit/main/docs/",
+      },
+      customCss: [
+        "@fontsource/geist/400.css",
+        "@fontsource/geist/600.css",
+        "./src/styles/app.css",
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
