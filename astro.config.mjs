23d83a8e282e69f9538bc3713cb4cdc1af6789e4
Chung-Hsuan Tsai <zx.c@nycu.edu.tw>
2025-11-15 18:02:22 +0800

    feat: update starlight configuration (#8)
    * feat: configure starlight integration and add logo assets
    
    * feat: add Prettier configuration and ignore files
    
    * feat: integrate Tailwind CSS and update styles
    
    * feat: enhance Starlight integration with OpenAPI support and update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,67 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://otterscale.com',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'OtterScale',
+			logo: {
+				src: './src/assets/logo.png'
+			},
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/otterscale/otterscale'
+				}
+			],
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en'
+				},
+				'zh-hant': {
+					label: '繁體中文',
+					lang: 'zh-Hant'
+				}
+			},
+			editLink: {
+				baseUrl: 'https://github.com/otterscale/otterscale.github.io/edit/main/docs/'
+			},
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+						{ label: 'Example Guide', slug: 'guides/example' }
+					]
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					autogenerate: { directory: 'reference' }
 				},
+				...openAPISidebarGroups
 			],
-		}),
+			customCss: ['./src/styles/global.css'],
+			plugins: [
+				// Generate the OpenAPI documentation pages.
+				starlightOpenAPI([
+					{
+						base: 'api',
+						schema:
+							'https://raw.githubusercontent.com/otterscale/otterscale/refs/heads/main/api/openapi.yaml'
+					}
+				])
+			]
+		})
 	],
+	vite: {
+		plugins: [tailwindcss()]
+	}
 });
